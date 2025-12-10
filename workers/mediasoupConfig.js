df44ee438bcbc05c48231a0532cbd4c2805e2590
thanhtamdak <127088717+thanhtamdak@users.noreply.github.com>
2025-12-10 11:18:46 +0700

    Create mediasoupConfig.js

---
+++ workers/mediasoupConfig.js
@@ -0,0 +1,47 @@
+// workers/mediasoupConfig.js
+module.exports = {
+  worker: {
+    logLevel: 'debug',
+    logTags: ['info', 'ice', 'dtls', 'rtp', 'srtp', 'rtcp'],
+    rtcMinPort: parseInt(process.env.MEDIASOUP_MIN_PORT || 20000),
+    rtcMaxPort: parseInt(process.env.MEDIASOUP_MAX_PORT || 29999)
+  },
+  router: {
+    mediaCodecs: [
+      {
+        kind: 'audio',
+        mimeType: 'audio/opus',
+        clockRate: 48000,
+        channels: 2
+      },
+      {
+        kind: 'video',
+        mimeType: 'video/VP8',
+        clockRate: 90000
+      },
+      {
+        kind: 'video',
+        mimeType: 'video/H264',
+        clockRate: 90000,
+        parameters: {
+          'packetization-mode': 1,
+          'profile-level-id': '42e01f',
+          'level-asymmetry-allowed': 1
+        }
+      }
+    ]
+  },
+  webRtcTransport: {
+    listenIps: [
+      {
+        ip: '0.0.0.0',
+        announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP || null
+      }
+    ],
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+    maxIncomingBitrate: 1500000,
+    initialAvailableOutgoingBitrate: 1000000
+  }
+};
