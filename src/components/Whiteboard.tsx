782981ffb0593c0b36f9450b92ab0ad67ee0a636
thanhtamdak <127088717+thanhtamdak@users.noreply.github.com>
2025-12-10 11:22:06 +0700

    Create Whiteboard.tsx

---
+++ src/components/Whiteboard.tsx
@@ -0,0 +1,9 @@
+// src/components/Whiteboard.tsx - Using tldraw
+import { Tldraw } from '@tldraw/tldraw';
+import '@tldraw/tldraw/tldraw.css';
+
+const Whiteboard = () => {
+  return <Tldraw />;
+};
+
+export default Whiteboard;
